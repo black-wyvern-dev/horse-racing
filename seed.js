6a60042a26a69bf9072ec343094578de83b40adc
Alex1992818 <meanJustin@protonmail.com>
2021-04-07 01:07:52 +0900

    seed

--- seed.js
+++ seed.js
@@ -5,8 +5,10 @@ const { editResource, getResource } = require('./app/methods/resource');
 const { editCurRaceInfo, getCurRaceInfo } = require('./app/methods/curraceinfo');
 const { editNextRaceInfo, getNextRaceInfo } = require('./app/methods/nextraceinfo');
 const { insertBettingInfo, getBettingInfo } = require('./app/methods/bettinginfo');
-const url = 'mongodb://192.168.104.56:8001/horse-racing';
+// const url = 'mongodb://192.168.104.56:8001/horse-racing';
 // const url = 'mongodb://localhost:8001/horse-racing';
+const url = 'mongodb+srv://admin:%21QAZxsw2@puzzle.am9gf.mongodb.net/Horse_racing?authSource=admin&replicaSet=atlas-h19s4z-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
